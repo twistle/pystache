209fb7b92cfce6eb007ab90efc6f5634c6c419e8
David Ross <dave.ross@gmail.com>
2021-09-22 10:41:05 -0400

    drop use_2to3

--- setup.py
+++ setup.py
@@ -351,13 +351,7 @@ def get_extra_args():
 
     """
     extra = {}
-    # TODO: it might be more correct to check whether we are using
-    #   Distribute instead of setuptools, since use_2to3 doesn't take
-    #   effect when using Python 2, even when using Distribute.
-    if py_version >= (3, ):
-        # Causes 2to3 to be run during the build step.
-        extra['use_2to3'] = True
-
+    
     return extra
 
 
